7fbb2b03898f74b8bd3e0c753431b2d59c82b96c
Bruno Alves <bruno-alves2010@live.com>
2023-07-15 19:08:04 -0300

    fix: fix console errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+'use client';
+
 import Button from '@/lib/button';
 import ToggleTheme from '@/lib/toggle-theme';
 import Checkbox from '@/lib/checkbox';
 import TextField from '@/lib/text-field';
 import TextArea from '@/lib/text-area';
 import Dropdown from '@/lib/dropdown';
+import { useState } from 'react';
 
 export default function Home() {
+  const [isCheck, setIsChecked] = useState(true);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex gap-4">
@@ -21,7 +26,8 @@ export default function Home() {
       <div className="bg-white dark:bg-grey-500 flex gap-4 h-40 w-1/2 items-center justify-center px-4">
         <Checkbox
           label="Talk to potential customers about our proposed solution and ask for fair price expectancy"
-          checked
+          checked={isCheck}
+          onChange={() => setIsChecked(!isCheck)}
         />
       </div>
 

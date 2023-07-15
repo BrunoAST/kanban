5dadb0b5c79374ca121531d578e09e5cea5116fc
Bruno Alves <bruno-alves2010@live.com>
2023-07-15 17:15:51 -0300

    style: remove unnecessary styling

--- src/lib/text-area/index.tsx
+++ src/lib/text-area/index.tsx
@@ -13,10 +13,9 @@ export default function TextArea({ placeholder, label }: TextAreaProps) {
         {label}
       </label>
       <textarea
-        className="form-textarea text-[13px] placeholder-shown:text-grey-700  placeholder-shown:text-opacity-100
-          font-medium py-2 px-4 rounded text-grey-700 border-grey-300 focus:opacity-100 invalid:border-red-200
-          invalid:opacity-100 invalid:focus:ring-red-200 focus:ring-purple-200 w-full bg-transparent
-          dark:text-white"
+        className="form-textarea text-[13px] placeholder-shown:text-grey-700 placeholder-shown:text-opacity-100
+          font-medium py-2 px-4 rounded text-grey-700 border-grey-300 focus:opacity-100
+          focus:ring-purple-200 w-full bg-transparent dark:text-white"
         placeholder={placeholder}
         rows={4}
         id="area"

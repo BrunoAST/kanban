eab2131a9471ba3f2fdda0f5593b188093e0e331
Bruno Alves <bruno-alves2010@live.com>
2023-07-15 21:13:05 -0300

    chore: update storybook preview content

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,20 @@ import type { Preview } from "@storybook/react";
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
+    backgrounds: {
+      default: 'light',
+      values: [
+        {
+          name: 'light',
+          value: '#fff',
+        },
+        {
+          name: 'dark',
+          value: '#2B2C37',
+        },
+      ],
+    },
+    layout: 'centered',
     controls: {
       matchers: {
         color: /(background|color)$/i,

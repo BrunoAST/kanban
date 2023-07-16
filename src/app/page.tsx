af9541a707a9a512e9f1c1aac4cb8a1b7baf7235
Bruno Alves <bruno-alves2010@live.com>
2023-07-15 23:31:30 -0300

    refactor: remove textfield from page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
-'use client';
-
 import Button from '@/lib/button';
 import ToggleTheme from '@/lib/toggle-theme';
-import Checkbox from '@/lib/checkbox';
-import TextField from '@/lib/text-field';
 import TextArea from '@/lib/text-area';
 import Dropdown from '@/lib/dropdown';
-import { useState } from 'react';
 
 export default function Home() {
-  const [isCheck, setIsChecked] = useState(true);
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex gap-4">
@@ -21,10 +14,6 @@ export default function Home() {
 
       <div className="flex gap-4 h-40 w-1/2 bg-grey-500 items-center justify-center">
         <ToggleTheme />
-      </div>
-
-      <div className="bg-white dark:bg-grey-500 flex gap-4 h-40 w-1/2 items-center justify-center px-4">
-        <TextField placeholder='Enter task name' />
       </div>
 
       <div className="bg-white dark:bg-grey-500 flex gap-4 h-40 w-1/2 items-center justify-center px-4">

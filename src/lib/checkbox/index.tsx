7af7b691a68d91d3e7271420bb3aac0d2f5ae046
Bruno Alves <bruno-alves2010@live.com>
2023-07-15 21:44:53 -0300

    feat: add new props to checkbox

--- src/lib/checkbox/index.tsx
+++ src/lib/checkbox/index.tsx
@@ -1,15 +1,29 @@
-import { InputHTMLAttributes } from 'react';
-
-type CheckboxProps = & InputHTMLAttributes<HTMLInputElement> & {
+type CheckboxProps = {
   /**
    * Text that is displayed next to the checkbox.
    * It is a required prop and must be a string.
    * The default value is an empty string.
    */
   label: string;
+
+  /**
+   * Used to set the initial state of the checkbox.
+   * It can also be used to change the state of the checkbox dynamically.
+   */
+  checked?: boolean;
+
+  /**
+   * Used to handle the change event of the checkbox.
+   * It can be used to update the state of the checkbox, or to perform other actions.
+   */
+  onChange?: () => void;
 };
 
-export default function Checkbox({ label, ...rest }: CheckboxProps) {
+export default function Checkbox({
+  label,
+  checked,
+  onChange 
+}: CheckboxProps) {
   return (
     <div className="w-full inline-flex items-center gap-4 p-3 rounded duration-300
       bg-grey-100 hover:bg-purple-150 dark:hover:bg-purple-150 dark:bg-grey-600"
@@ -19,7 +33,8 @@ export default function Checkbox({ label, ...rest }: CheckboxProps) {
         type="checkbox"
         className="form-checkbox peer h-4 w-4 checked:text-purple-200 bg-white rounded border-grey-250
           [&:not(:checked)]:dark:bg-grey-500 focus:ring-purple-200 focus:ring-2 focus:ring-offset-1"
-        {...rest}
+        checked={checked}
+        onChange={onChange}
       />
       <label
         htmlFor="checkbox"

7084d0b4425ceb4971531d73140e1e3abe6b4162
Bruno Alves <bruno-alves2010@live.com>
2023-07-21 19:32:03 -0300

    refactor: remove button from page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
 'use client';
 
-import Button from '@/lib/button';
-import ToggleTheme from '@/lib/toggle-theme';
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex gap-4">
-        <Button label="+ Add New Column" variant='primary' />
-        <Button label="+ Add New Column" variant='secondary' />
-        <Button label="+ Add New Column" variant='destructive' />
-      </div>
     </main>
   );
 }

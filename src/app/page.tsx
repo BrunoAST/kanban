0bbcf904dafa103dc38a806c102524d96e3c26b7
Bruno Alves <bruno-alves2010@live.com>
2023-07-30 16:13:45 -0300

    styles: remove unnecessary styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    </main>
+    <div className="flex flex-col">
+      Main Content
+    </div>
   );
 }

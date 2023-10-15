fcf3e668264ac0a1025c9b5273109d365b4d67fe
Bruno Alves <bruno-alves2010@live.com>
2023-10-15 13:27:21 -0300

    docs: create story for icons

---
+++ src/stories/Icons.stories.tsx
@@ -0,0 +1,27 @@
+import Icons from '../lib/icons';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof Icons> = {
+  title: 'Components/Icons',
+  component: Icons,
+  tags: ['autodocs'],
+  argTypes: {
+    classes: {
+      control: 'text'
+    },
+    name: {
+      control: 'select',
+      options: ['board', 'hide', 'chevron-down']
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Icons>;
+
+export const Default: Story = {
+  args: {
+    classes: 'fill-purple-200',
+    name: 'board'
+  },
+};

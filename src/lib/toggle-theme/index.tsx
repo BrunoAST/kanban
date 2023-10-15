df6430ba0e899cb1f21811f7f7cc00e7a50bd265
Bruno Alves <bruno-alves2010@live.com>
2023-10-15 13:40:04 -0300

    feat: add hover effect

--- src/lib/toggle-theme/index.tsx
+++ src/lib/toggle-theme/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import useTheme from '@/hooks/use-theme';
-import Image from 'next/image';
+import Icons from '../icons';
 
 export default function ToggleTheme() {
   const { handleDarkTheme, isDarkModeEnabled } = useTheme();
 
   return (
-    <div className="relative inline-flex gap-6 bg-grey-100 dark:bg-grey-600 h-12 px-14 rounded-md">
-      <Image src="/icons/Light.svg" alt="Light mode" width="18" height="18" />
+    <div className="relative inline-flex items-center gap-6 bg-grey-100 dark:bg-grey-600 h-12 px-14 rounded-md">
+      <Icons name="light" />
 
       <label className="relative inline-flex items-center cursor-pointer">
         <input
@@ -16,13 +16,13 @@ export default function ToggleTheme() {
           onChange={e => handleDarkTheme(e.target.checked)}
           className="sr-only peer"
         />
-        <span className="w-11 h-6 bg-purple-200 rounded-full peer-checked:after:translate-x-full
+        <span className="w-11 h-6 bg-purple-200 hover:bg-purple-100 rounded-full peer-checked:after:translate-x-full
           peer-checked:after:border-white after:absolute after:top-[50%] after:left-[2px] after:bg-white
           after:translate-y-[-50%] after:rounded-full after:h-5 after:w-5 after:transition-all"
         />
       </label>
 
-      <Image src="/icons/Dark.svg" alt="Dark mode" width="18" height="18" />
+      <Icons name="dark" />
     </div>
   );
 }

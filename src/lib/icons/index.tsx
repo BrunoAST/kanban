577d1464a1fbfa2beddd11470085afede588171e
Bruno Alves <bruno-alves2010@live.com>
2023-10-15 14:54:56 -0300

    docs: update description of icon's props

--- src/lib/icons/index.tsx
+++ src/lib/icons/index.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import { ICON_MAP } from './icon-map';
 
 type IconsProps = {
+  /**
+  * List with available icons.
+  */
   name: 'board' | 'hide' | 'chevron-down' | 'light' | 'dark';
+
+  /**
+  * Custom classes that is going to be applied to the svg element when provided.
+  */
   classes?: string;
 };
 

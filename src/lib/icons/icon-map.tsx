246bb45a66626f5e6e5cd0b9e56229178755ad41
Bruno Alves <bruno-alves2010@live.com>
2023-10-17 19:40:28 -0300

    feat: add plus icon

--- src/lib/icons/icon-map.tsx
+++ src/lib/icons/icon-map.tsx
@@ -86,5 +86,18 @@ export const ICON_MAP = [
       <circle cx="2.30769" cy="10.0001" r="2.30769" />
       <circle cx="2.30769" cy="17.6922" r="2.30769" />
     </svg>
+  },
+  {
+    name: 'plus',
+    content: (classes = 'fill-white') => <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={classes}
+    >
+      <path d="M7.368 12V7.344H12V4.632H7.368V0H4.656V4.632H0V7.344H4.656V12H7.368Z" />
+    </svg>
   }
 ];
